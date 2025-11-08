7c1d776dab4ca183cf52ac4fa4fc538ea17e6c62
Muzammil Makandar <21ST114282046@sanjayghodawatuniversity.ac.in>
2025-11-08 18:30:42 +0530

    fix build + convert to SPA

--- src/lib/redux/StoreProvider.tsx
+++ src/lib/redux/StoreProvider.tsx
@@ -1,18 +1,11 @@
-'use client';
-import { useRef, type ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { makeStore, type AppStore } from './store';
+import store from './store';
 
 interface StoreProviderProps {
   children: ReactNode;
 }
 
 export function StoreProvider({ children }: StoreProviderProps) {
-  const storeRef = useRef<AppStore | null>(null);
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore();
-  }
-
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return <Provider store={store}>{children}</Provider>;
 }
